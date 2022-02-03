decc1979437f7f7e35425a7901afdb7b16006242
Safat99 <sarkersafat99@gmail.com>
2022-02-03 15:13:25 +0600

    connected to the local mongoDB successfully

---
+++ test_project/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func close(client *mongo.Client, ctx context.Context, cancel context.CancelFunc) {
+	defer cancel()
+	defer func() {
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, error) {
+	// ctx will be used to set deadline for process, here deadline will of 30 secs
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	// mongo.Connect return mongo.Client method
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+
+}
+
+func ping(client *mongo.Client, ctx context.Context) error {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected succesfully")
+	return nil
+}
+
+func main() {
+	client, ctx, cancel, err := connect("mongodb://localhost:27017")
+	if err != nil {
+		panic(err)
+	}
+
+	defer close(client, ctx, cancel)
+	ping(client, ctx)
+
+}
