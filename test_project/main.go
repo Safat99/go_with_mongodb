f1b069966b978cbe4bd3a53c0ba82aa0654bcda9
Safat99 <sarkersafat99@gmail.com>
2022-02-03 18:14:02 +0600

    inserting to local  database done

--- test_project/main.go
+++ test_project/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -28,6 +29,21 @@ func connect(uri string) (*mongo.Client, context.Context, context.CancelFunc, er
 
 }
 
+// select database and collection with Client.Database method
+// and Database.Collection method
+func insertOne(client *mongo.Client, ctx context.Context, dataBase, col string, doc interface{}) (*mongo.InsertOneResult, error) {
+	collection := client.Database(dataBase).Collection(col)
+	result, err := collection.InsertOne(ctx, doc)
+	return result, err
+}
+
+func insertMany(client *mongo.Client, ctx context.Context, dataBase, col string, docs []interface{}) (*mongo.InsertManyResult, error) {
+	collection := client.Database(dataBase).Collection(col)
+
+	result, err := collection.InsertMany(ctx, docs)
+	return result, err
+}
+
 func ping(client *mongo.Client, ctx context.Context) error {
 	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		return err
@@ -45,4 +61,52 @@ func main() {
 	defer close(client, ctx, cancel)
 	ping(client, ctx)
 
+	// creating a object of type interface to store
+	// the bson values, that we are inserting into database
+	var document interface{}
+
+	document = bson.D{
+		{"rollNo", 175},
+		{"maths", 80},
+		{"science", 90},
+		{"computer", 95},
+	}
+
+	insertOneResult, err := insertOne(client, ctx, "gfg", "marks", document)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Result of InsertOne")
+	fmt.Println(insertOneResult.InsertedID)
+
+	var documents []interface{}
+
+	documents = []interface{}{
+		bson.D{
+			{"rollNo", 153},
+			{"maths", 65},
+			{"science", 59},
+			{"computer", 55},
+		},
+		bson.D{
+			{"rollNo", 162},
+			{"maths", 86},
+			{"science", 80},
+			{"computer", 69},
+		},
+	}
+
+	insertManyResult, err := insertMany(client, ctx, "gfg", "marks", documents)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Result of InsertMany")
+
+	for id := range insertManyResult.InsertedIDs {
+		fmt.Println(id)
+	}
 }
